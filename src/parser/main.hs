3124c9abdec6083c665beb6997003c42d9df10e5
Cris24dc <cristianandrei752@gmail.com>
2025-03-15 23:26:53 +0200

    empty json bug fixed

--- src/parser/main.hs
+++ src/parser/main.hs
@@ -1,13 +1,10 @@
--- Definim tipurile pentru AST
-
-import System.IO
-
-data SQLStatement = 
+import Data.List (isSuffixOf)
+data SQLStatement =
       SelectStmt [String] String (Maybe Condition)
     | CreateStmt String [(String, String)]
     | InsertStmt String [String] [String]
     | UpdateStmt String [(String, String)] (Maybe Condition)
-    | DeleteStmt String (Maybe Condition)
+    | DropStmt String
     deriving (Show)
 
 data Condition = Equals String String | GreaterThan String String | LessThan String String | And Condition Condition | Or Condition Condition | Not Condition
@@ -15,16 +12,16 @@ data Condition = Equals String String | GreaterThan String String | LessThan Str
 
 -- Functie pentru serializare JSON simpla
 serializeJSON :: SQLStatement -> String
-serializeJSON (SelectStmt cols table cond) = 
-    "{\"type\": \"SELECT\", \"columns\": " ++ show cols ++ ", \"table\": \"" ++ table ++ "\"" ++ maybe "" (", \"where\": " ++) (fmap serializeCond cond) ++ "}"
-serializeJSON (CreateStmt table cols) = 
+serializeJSON (SelectStmt cols table cond) =
+    "{\"type\": \"SELECT\", \"columns\": " ++ (if cols == ["*"] then "\"*\"" else show cols) ++ ", \"table\": \"" ++ table ++ "\"" ++ maybe "" (", \"where\": " ++) (fmap serializeCond cond) ++ "}"
+serializeJSON (CreateStmt table cols) =
     "{\"type\": \"CREATE\", \"table\": \"" ++ table ++ "\", \"columns\": " ++ show cols ++ "}"
-serializeJSON (InsertStmt table cols values) = 
+serializeJSON (InsertStmt table cols values) =
     "{\"type\": \"INSERT\", \"table\": \"" ++ table ++ "\", \"columns\": " ++ show cols ++ ", \"values\": " ++ show values ++ "}"
-serializeJSON (UpdateStmt table updates cond) = 
+serializeJSON (UpdateStmt table updates cond) =
     "{\"type\": \"UPDATE\", \"table\": \"" ++ table ++ "\", \"updates\": " ++ show updates ++ maybe "" (", \"where\": " ++) (fmap serializeCond cond) ++ "}"
-serializeJSON (DeleteStmt table cond) = 
-    "{\"type\": \"DELETE\", \"table\": \"" ++ table ++ "\"" ++ maybe "" (", \"where\": " ++) (fmap serializeCond cond) ++ "}"
+serializeJSON (DropStmt table) =
+    "{\"type\": \"DROP\", \"table\": \"" ++ table ++ "\"}"
 
 serializeCond :: Condition -> String
 serializeCond (Equals col val) = "{\"equals\": [\"" ++ col ++ "\", \"" ++ val ++ "\"]}"
@@ -36,19 +33,27 @@ serializeCond (Not c) = "{\"not\": " ++ serializeCond c ++ "}"
 
 -- Parser simplu
 parseSQL :: String -> Maybe SQLStatement
-parseSQL input = case words input of
-    ("SELECT":cols) -> let (columnList, rest) = break (== "FROM") cols in
-                        case rest of
-                            (_:table:whereClause) -> let (whereKey, whereExpr) = break (== "WHERE") whereClause in
-                                                      Just (SelectStmt (map (filter (/= ',')) columnList) table (parseWhere (drop 1 whereExpr)))
-                            _ -> Nothing
-    ("CREATE":"TABLE":table:cols) -> Just (CreateStmt table (parseColumns cols))
-    ("INSERT":"INTO":table:values) -> let (colPart, valPart) = break (== "VALUES") values in
-                                        Just (InsertStmt table colPart (tail valPart))
-    ("UPDATE":table:updates) -> let (setPart, wherePart) = break (== "WHERE") updates in
-                                 Just (UpdateStmt table (parseSet (unwords setPart)) (parseWhere (tail wherePart)))
-    ("DELETE":"FROM":table:whereClause) -> Just (DeleteStmt table (parseWhere whereClause))
-    _ -> Nothing
+parseSQL input =
+    let trimmedInput = reverse (dropWhile (== ' ') (reverse input)) in
+    if not (";" `isSuffixOf` trimmedInput)
+        then Nothing
+        else case words (init trimmedInput) of
+            ("SELECT":"*":"FROM":table:whereClause) ->
+                Just (SelectStmt ["*"] table (parseWhere whereClause))
+            ("SELECT":cols) -> let (columnList, rest) = break (== "FROM") cols in
+                                if "*" `elem` columnList && length columnList > 1
+                                    then Nothing
+                                    else case rest of
+                                        (_:table:whereClause) -> let (whereKey, whereExpr) = break (== "WHERE") whereClause in
+                                                                  Just (SelectStmt (map (filter (/= ',')) columnList) table (parseWhere (drop 1 whereExpr)))
+                                        _ -> Nothing
+            ("CREATE":"TABLE":table:cols) -> Just (CreateStmt table (parseColumns cols))
+            ("INSERT":"INTO":table:values) -> let (colPart, valPart) = break (== "VALUES") values in
+                                                Just (InsertStmt table colPart (tail valPart))
+            ("UPDATE":table:updates) -> let (setPart, wherePart) = break (== "WHERE") updates in
+                                         Just (UpdateStmt table (parseSet (unwords setPart)) (parseWhere (tail wherePart)))
+            ("DROP":"TABLE":table:_) -> Just (DropStmt table)
+            _ -> Nothing
 
 parseColumns :: [String] -> [(String, String)]
 parseColumns cols = [(c, "TEXT") | c <- cols]
@@ -70,14 +75,16 @@ splitOn delim xs = case break (== delim) xs of
     (before, []) -> [before]
     (before, _:after) -> before : splitOn delim after
 
+
 -- Main: Citire input si scriere JSON
 main :: IO ()
 main = do
-    putStrLn "Introduceti un query SQL:"
     query <- getLine
     case parseSQL query of
         Just ast -> do
             let jsonOutput = serializeJSON ast
             writeFile "output.json" jsonOutput
-            putStrLn "Fisierul output.json a fost generat."
-        Nothing -> putStrLn "Parse error"
+            putStrLn "Query OK"
+        Nothing -> do
+            writeFile "output.json" ""
+            putStrLn "Unknown SQL Statement"
