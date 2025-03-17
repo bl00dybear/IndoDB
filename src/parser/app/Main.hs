685ede586020d5760c9d1372ba5d04200e59c3dc
Cris24dc <cristianandrei752@gmail.com>
2025-03-17 13:30:24 +0200

    cabal project done

---
+++ src/parser/app/Main.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+import GHC.Generics (Generic)
+import Data.Aeson (ToJSON, encode)
+import qualified Data.ByteString.Lazy.Char8 as B
+import Text.Parsec
+import Text.Parsec.String
+import Control.Monad (void)
+
+-- Definirea tipurilor de date pentru AST
+
+data SQLStatement
+    = SelectStmt [String] String (Maybe Condition)
+    | CreateStmt String [(String, String)]
+    | InsertStmt String [String] [String]
+    | UpdateStmt String [(String, String)] (Maybe Condition)
+    | DropStmt String
+    deriving (Show, Generic, ToJSON)
+
+data Condition
+    = Equals String String
+    | GreaterThan String String
+    | LessThan String String
+    | And Condition Condition
+    | Or Condition Condition
+    | Not Condition
+    deriving (Show, Generic, ToJSON)
+
+-- Definirea parserului folosind Parsec
+
+identifier :: Parser String
+identifier = many1 (letter <|> digit <|> char '_')
+
+parseSelect :: Parser SQLStatement
+parseSelect = do
+    void $ string "SELECT "
+    cols <- sepBy identifier (string ", ")
+    void $ string " FROM "
+    table <- identifier
+    cond <- optionMaybe parseWhere
+    return $ SelectStmt cols table cond
+
+parseCreate :: Parser SQLStatement
+parseCreate = do
+    void $ string "CREATE TABLE "
+    table <- identifier
+    cols <- between (char '(') (char ')') (sepBy ((,) <$> identifier <*> identifier) (string ", "))
+    return $ CreateStmt table cols
+
+parseInsert :: Parser SQLStatement
+parseInsert = do
+    void $ string "INSERT INTO "
+    table <- identifier
+    cols <- between (char '(') (char ')') (sepBy identifier (string ", "))
+    void $ string " VALUES "
+    values <- between (char '(') (char ')') (sepBy identifier (string ", "))
+    return $ InsertStmt table cols values
+
+parseUpdate :: Parser SQLStatement
+parseUpdate = do
+    void $ string "UPDATE "
+    table <- identifier
+    void $ string " SET "
+    updates <- sepBy ((,) <$> identifier <*> (string " = " *> identifier)) (string ", ")
+    cond <- optionMaybe parseWhere
+    return $ UpdateStmt table updates cond
+
+parseDrop :: Parser SQLStatement
+parseDrop = do
+    void $ string "DROP TABLE "
+    DropStmt <$> identifier
+
+parseWhere :: Parser Condition
+parseWhere = do
+    void $ string " WHERE "
+    Equals <$> identifier <*> (string " = " *> identifier)
+
+parseSQL :: Parser SQLStatement
+parseSQL = try parseSelect <|> try parseCreate <|> try parseInsert <|> try parseUpdate <|> parseDrop
+
+main :: IO ()
+main = do
+    input <- getLine
+    case parse parseSQL "SQL statement" input of
+        Left err -> putStrLn $ "unknown command: " ++ show err
+        Right ast -> do
+            let jsonOutput = encode ast
+            B.writeFile "output.json" jsonOutput
+
