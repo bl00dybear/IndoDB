bb83ae3e4bde2dab9d4df945cbd9488a321c6c10
Cris24dc <cristianandrei752@gmail.com>
2025-05-04 23:46:07 +0300

    main bug fixed

--- src/main.c
+++ src/main.c
@@ -9,114 +9,6 @@
 #include "../include/utils/globals.h"
 #include "../include/utils/queue.h"
 
-void parse_statement(const char *filename, Statement *stmt) {
-    FILE *fp = fopen(filename, "r");
-    if (!fp) {
-        perror("Failed to open JSON file");
-        exit(1);
-    }
-
-    fseek(fp, 0, SEEK_END);
-    long length = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    char *data = malloc(length + 1);
-    fread(data, 1, length, fp);
-    data[length] = '\0';
-    fclose(fp);
-
-    printf("A ajuns in parse statement\n\n");
-
-
-    cJSON *json = cJSON_Parse(data);
-    if (!json) {
-        printf("Error parsing JSON\n");
-        exit(1);
-    }
-
-    printf("aici1\n\n");
-
-    cJSON *statement_type = cJSON_GetObjectItemCaseSensitive(json, "statement");
-
-    printf("aici2\n\n");
-
-    if (strcmp(statement_type->valuestring, "InsertStmt") == 0) {
-
-        printf("aici3\n\n");
-
-        stmt->type = STATEMENT_INSERT;
-
-        printf("aici4\n\n");
-
-        cJSON *columns = cJSON_GetObjectItemCaseSensitive(json, "columns");
-        cJSON *table = cJSON_GetObjectItemCaseSensitive(json, "table");
-        cJSON *values = cJSON_GetObjectItemCaseSensitive(json, "values");
-
-        int num_columns = cJSON_GetArraySize(columns);
-        int num_values = cJSON_GetArraySize(values);
-
-        stmt->insertStmt.num_columns = num_columns;
-        stmt->insertStmt.num_values = num_values;
-
-        printf("aici5\n\n");
-
-        stmt->insertStmt.columns = malloc(num_columns * sizeof(char*));
-        for (int i = 0; i < num_columns; i++) {
-            cJSON *col = cJSON_GetArrayItem(columns, i);
-            stmt->insertStmt.columns[i] = strdup(col->valuestring);
-        }
-
-        stmt->insertStmt.table = strdup(table->valuestring);
-
-        stmt->insertStmt.values = malloc(num_values * sizeof(*stmt->insertStmt.values));
-        for (int i = 0; i < num_values; i++) {
-            cJSON *val_obj = cJSON_GetArrayItem(values, i);
-            cJSON *val = cJSON_GetObjectItemCaseSensitive(val_obj, "value");
-            cJSON *valType = cJSON_GetObjectItemCaseSensitive(val_obj, "valueType");
-
-            if (cJSON_IsString(val)) {
-                stmt->insertStmt.values[i].value = strdup(val->valuestring);
-            } else if (cJSON_IsNumber(val)) {
-                char buffer[32];
-                snprintf(buffer, sizeof(buffer), "%g", val->valuedouble);
-                stmt->insertStmt.values[i].value = strdup(buffer);
-            } else {
-                printf("Unsupported value type in JSON!\n");
-                exit(1);
-            }
-
-            stmt->insertStmt.values[i].valueType = strdup(valType->valuestring);
-        }
-    } else if (strcmp(statement_type->valuestring, "SelectStmt") == 0) {
-        stmt->type = STATEMENT_SELECT;
-        cJSON *columns = cJSON_GetObjectItemCaseSensitive(json, "columns");
-        cJSON *table = cJSON_GetObjectItemCaseSensitive(json, "table");
-        cJSON *condition = cJSON_GetObjectItemCaseSensitive(json, "condition");
-
-        int num_columns = cJSON_GetArraySize(columns);
-        stmt->selectStmt.num_columns = num_columns;
-
-        stmt->selectStmt.columns = malloc(num_columns * sizeof(char*));
-        for (int i = 0; i < num_columns; i++) {
-            cJSON *col = cJSON_GetArrayItem(columns, i);
-            stmt->selectStmt.columns[i] = strdup(col->valuestring);
-        }
-
-        stmt->selectStmt.table = strdup(table->valuestring);
-
-        if (cJSON_IsNull(condition)) {
-            stmt->selectStmt.condition = NULL;
-        } else {
-            stmt->selectStmt.condition = strdup(condition->valuestring);
-        }
-    } else {
-        printf("Unknown statement type: %s\n", statement_type->valuestring);
-        exit(1);
-    }
-
-    cJSON_Delete(json);
-    free(data);
-}
-
 void free_statement(Statement *stmt) {
     if (stmt->type == STATEMENT_INSERT) {
         for (int i = 0; i < stmt->insertStmt.num_columns; i++) {
@@ -142,6 +34,276 @@ void free_statement(Statement *stmt) {
         }
     }
 }
+
+void parse_statement(const char *filename, Statement *stmt) {
+    // Este deschis fisierul output.json
+    FILE *fp = fopen(filename, "r");
+    // Error Handling
+    if (!fp) {
+        perror("Failed to open JSON file");
+        exit(1);
+    }
+
+    // Cauta finalul fisierul
+    fseek(fp, 0, SEEK_END);
+    // Ia dimensiunea acestuia
+    long length = ftell(fp);
+    // Cauta inceputul fisierului
+    fseek(fp, 0, SEEK_SET);
+    // Aloca memorie pentru a stoca
+    // continutul fisierului in data
+    char *data = malloc(length + 1);
+    // Citeste length bytes(1) in data
+    fread(data, 1, length, fp);
+    // Pune terminatorul de string
+    data[length] = '\0';
+    // Inchide fisierul
+    fclose(fp);
+
+
+    // Parsam data si stocam in json
+    cJSON *json = cJSON_Parse(data);
+    // Error Handling
+    if (!json) {
+        printf("Error parsing JSON\n");
+        exit(1);
+    }
+
+    // Selectam ce tip de statement este
+    cJSON *statement_type = cJSON_GetObjectItemCaseSensitive(json, "statement");
+    // printf("Tip de stmt: %s\n", statement_type->valuestring);
+
+    // Initializam statementul in functie de ce tip este:
+    if (strcmp(statement_type->valuestring, "InsertStmt") == 0) {
+        stmt->type = STATEMENT_INSERT;
+
+        cJSON *columns = cJSON_GetObjectItemCaseSensitive(json, "columns");
+        cJSON *table = cJSON_GetObjectItemCaseSensitive(json, "table");
+        cJSON *values = cJSON_GetObjectItemCaseSensitive(json, "values");
+
+        int num_columns = cJSON_GetArraySize(columns);
+        int num_values = cJSON_GetArraySize(values);
+
+        stmt->insertStmt.num_columns = num_columns;
+        stmt->insertStmt.num_values = num_values;
+        // printf("Nr. coloane: %d\n", stmt->insertStmt.num_columns);
+        // printf("Nr. valori: %d\n", stmt->insertStmt.num_values);
+
+        stmt->insertStmt.columns = malloc(num_columns * sizeof(char*));
+        for (int i = 0; i < num_columns; i++) {
+            cJSON *col = cJSON_GetArrayItem(columns, i);
+            stmt->insertStmt.columns[i] = strdup(col->valuestring);
+            // printf("Coloana: %s\n", stmt->insertStmt.columns[i]);
+        }
+
+        stmt->insertStmt.table = strdup(table->valuestring);
+        // printf("Nume tabel: %s\n", stmt->insertStmt.table);
+
+        stmt->insertStmt.values = malloc(num_values * sizeof(*stmt->insertStmt.values));
+        for (int i = 0; i < num_values; i++) {
+            cJSON *val_obj = cJSON_GetArrayItem(values, i);
+            cJSON *val = cJSON_GetObjectItemCaseSensitive(val_obj, "value");
+            cJSON *valType = cJSON_GetObjectItemCaseSensitive(val_obj, "valueType");
+
+            if (cJSON_IsString(val)) {
+                stmt->insertStmt.values[i].value = strdup(val->valuestring);
+                // printf("Value String: %s\n", stmt->insertStmt.values[i].value);
+            } else if (cJSON_IsNumber(val)) {
+                // se transforma int in char* pt ca asa e stocat in struct
+                char buffer[32];
+                snprintf(buffer, sizeof(buffer), "%g", val->valuedouble);
+                stmt->insertStmt.values[i].value = strdup(buffer);
+                // printf("Value Int: %s\n", stmt->insertStmt.values[i].value);
+            } else {
+                printf("Unsupported value type in JSON!\n");
+                exit(1);
+            }
+
+            stmt->insertStmt.values[i].valueType = strdup(valType->valuestring);
+            // printf("Value Type: %s\n", stmt->insertStmt.values[i].valueType);
+        }
+    } else if (strcmp(statement_type->valuestring, "SelectStmt") == 0) {
+        stmt->type = STATEMENT_SELECT;
+
+        cJSON *columns = cJSON_GetObjectItemCaseSensitive(json, "columns");
+        cJSON *table = cJSON_GetObjectItemCaseSensitive(json, "table");
+        cJSON *condition = cJSON_GetObjectItemCaseSensitive(json, "condition");
+
+        int num_columns = cJSON_GetArraySize(columns);
+
+        stmt->selectStmt.num_columns = num_columns;
+        // printf("Nr. coloane: %d\n", stmt->selectStmt.num_columns);
+        
+        stmt->selectStmt.columns = malloc(num_columns * sizeof(char*));
+        for (int i = 0; i < num_columns; i++) {
+            cJSON *col = cJSON_GetArrayItem(columns, i);
+            stmt->selectStmt.columns[i] = strdup(col->valuestring);
+            // printf("Coloana: %s\n", stmt->selectStmt.columns[i]);
+        }
+
+        stmt->selectStmt.table = strdup(table->valuestring);
+        // printf("Nume tabel: %s\n", stmt->selectStmt.table);
+
+        if (cJSON_IsNull(condition)) {
+            stmt->selectStmt.condition = NULL;
+            // printf("Conditia e NULL: %d", stmt->selectStmt.condition);
+        } else {
+            stmt->selectStmt.condition = strdup(condition->valuestring);
+        }
+    } else {
+        printf("Unknown statement type: %s\n", statement_type->valuestring);
+        exit(1);
+    }
+
+    cJSON_Delete(json);
+    free(data);
+}
+
+void process_statement(Statement *stmt) {
+    switch (stmt->type) {
+        case STATEMENT_INSERT: {
+            uint64_t row_size = 0;
+            char* row_content = get_row_content(stmt,&row_size);
+            printf("Row content: %ld\n",row_size);
+            void *written_address = write_row(df, row_content, row_size);
+            const uint64_t current_id = global_id++;
+            insert(current_id,written_address);
+            set_file_dirty_db(db, true);
+            set_file_dirty_df(df,true);
+
+
+            commit_changes_db(db);
+            commit_changes_df(df);
+        }
+        case STATEMENT_SELECT: {}
+        default: ;
+    }
+}
+
+int cli() {
+    char input[MAX_INPUT_SIZE];
+    char line[MAX_INPUT_SIZE];
+
+    // printf("\033[H\033[J");
+
+    while (1) {
+        printf("IndoDB> ");
+        input[0] = '\0';
+
+        while (fgets(line, sizeof(line), stdin)) {
+            // printf("A luat input\n\n");
+            size_t len = strlen(line);
+            if (len > 0 && line[len - 1] == '\n') {
+                line[len - 1] = '\0';
+                len--;
+            }
+
+            if (strcmp(line, "EXIT;") == 0) {
+                printf("Exiting IndoDB...\n");
+                return 0;
+            }
+
+            if (strlen(input) + strlen(line) + 2 < MAX_INPUT_SIZE) {
+                strcat(input, line);
+                strcat(input, " ");
+            } else {
+                printf("Input too long! Max supported length is %d characters.\n", MAX_INPUT_SIZE);
+                break;
+            }
+
+            char *trimmed = input + strlen(input) - 1;
+            while (trimmed >= input && *trimmed == ' ') {
+                trimmed--;
+            }
+            if (*trimmed == ';') {
+                break;
+            }
+
+            printf("     -> ");
+        }
+
+
+        if (strlen(input) > 0) {
+            FILE *fp = popen("../output/sql_parser", "w");
+            if (fp == NULL) {
+                perror("Error opening pipe");
+                return 1;
+            }
+            fprintf(fp, "%s\n", input);
+            pclose(fp);
+
+
+            Statement *stmt = malloc(sizeof(Statement));
+            if (!stmt) {
+                perror("malloc failed");
+                exit(1);
+            }
+            parse_statement("../output/output.json", stmt);
+
+            // TESTEAZA AICI
+            // TESTEAZA AICI
+            if (stmt->type == STATEMENT_INSERT) {
+                printf("INSERT into table: %s\n", stmt->insertStmt.table);
+                for (int i = 0; i < stmt->insertStmt.num_columns; i++) {
+                    printf("Column: %s\n", stmt->insertStmt.columns[i]);
+                }
+                for (int i = 0; i < stmt->insertStmt.num_values; i++) {
+                    printf("Value: %s (Type: %s)\n",
+                        stmt->insertStmt.values[i].value,
+                        stmt->insertStmt.values[i].valueType);
+                }
+            } else if (stmt->type == STATEMENT_SELECT) {
+                printf("SELECT from table: %s\n", stmt->selectStmt.table);
+                for (int i = 0; i < stmt->selectStmt.num_columns; i++) {
+                    printf("Column: %s\n", stmt->selectStmt.columns[i]);
+                }
+                if (stmt->selectStmt.condition) {
+                    printf("Condition: %s\n", stmt->selectStmt.condition);
+                } else {
+                    printf("No condition\n");
+                }
+            }
+
+
+            if (stmt != NULL) {
+                process_statement(stmt);
+                if (stmt->type == STATEMENT_INSERT) {
+                    printf("Parsed an INSERT statement!\n");
+                } else if (stmt->type == STATEMENT_SELECT) {
+                    printf("Parsed a SELECT statement!\n");
+                }
+                free_statement(stmt);
+            }else {
+                printf("Failed to parse statement!\n");
+                exit(EXIT_FAILURE);
+            }
+
+        }
+    }
+}
+
+int main() {
+    database_init();
+    return cli();
+}
+
+
+
+
+
+
+
+
+// ####### COD SEBI #######:
+
+
+
+
+
+
+
+
+
 
 
 void free_memory(Statement* stmt) {
@@ -173,100 +335,10 @@ void free_memory(Statement* stmt) {
     global_id = 1;
 }
 
-void process_statement(Statement *stmt) {
-    switch (stmt->type) {
-        case STATEMENT_INSERT: {
-            uint64_t row_size = 0;
-            char* row_content = get_row_content(stmt,&row_size);
-            printf("Row content: %ld\n",row_size);
-            void *written_address = write_row(df, row_content, row_size);
-            const uint64_t current_id = global_id++;
-            insert(current_id,written_address);
-            set_file_dirty_db(db, true);
-            set_file_dirty_df(df,true);
 
 
-            commit_changes_db(db);
-            commit_changes_df(df);
-        }
-        case STATEMENT_SELECT: {}
-        default: ;
-    }
-}
 
 
-int cli() {
-    char input[MAX_INPUT_SIZE];
-    char line[MAX_INPUT_SIZE];
-    printf("Welcome to IndoDB!\n");
-
-    // printf("\033[H\033[J");
-
-    while (1) {
-        printf("indodb$ ");
-        input[0] = '\0';
-
-        while (fgets(line, sizeof(line), stdin)) {
-            printf("A luat input\n\n");
-            size_t len = strlen(line);
-            if (len > 0 && line[len - 1] == '\n') {
-                line[len - 1] = '\0';
-                len--;
-            }
-
-            if (strcmp(line, "EXIT;") == 0) {
-                printf("Exiting IndoDB...\n");
-                return 0;
-            }
-
-            strcat(input, line);
-            strcat(input, " ");
-
-            char *trimmed = input + strlen(input) - 1;
-            while (trimmed >= input && *trimmed == ' ') {
-                trimmed--;
-            }
-            if (*trimmed == ';') {
-                break;
-            }
-
-            printf("      $ ");
-        }
-
-        printf("Aiurea moment\n\n");
-
-        if (strlen(input) > 0) {
-            FILE *fp = popen("../output/sql_parser", "w");
-            if (fp == NULL) {
-                perror("Error opening pipe");
-                return 1;
-            }
-            fprintf(fp, "%s\n", input);
-            pclose(fp);
-
-            printf("A parsat la haskell\n\n");
-
-            Statement *stmt ;
-            parse_statement("../output/output.json",stmt);
-
-            printf("A parsat la C\n\n");
-
-            if (stmt != NULL) {
-                process_statement(stmt);
-                if (stmt->type == STATEMENT_INSERT) {
-                    printf("Parsed an INSERT statement!\n");
-                } else if (stmt->type == STATEMENT_SELECT) {
-                    printf("Parsed a SELECT statement!\n");
-                }
-                free_statement(stmt);
-            }else {
-                printf("Failed to parse statement!\n");
-                exit(EXIT_FAILURE);
-            }
-
-        }
-    }
-}
 
 void database_init() {
     if(!((db = malloc(sizeof(DBFile))))){
@@ -324,14 +396,4 @@ void database_init() {
     for(int i=1;i<=10000;i+=1){
         push(free_page_queue,i);
     }
-}
-
-
-
-
-
-int main() {
-    database_init();
-
-    return cli();
 }
