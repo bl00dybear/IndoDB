f2e89186a452570e6c1c6b79fab389f615d7deb1
bloodybear <sincarisebastian@gmail.com>
2025-06-16 16:22:04 +0300

    utils

---
+++ src/utils/globals.c
@@ -0,0 +1,19 @@
+#include "../../include/utils/globals.h"
+#include "../../include/data/core_structures.h"
+
+RowNode *root = NULL;
+Queue* free_page_queue = NULL;
+NodeEntry visited_nodes[MAX_VISITED_NODES];
+int visited_count = 0;
+uint64_t serialized_pages[MAX_VISITED_NODES];
+int serialized_count = 0;
+DBFile* db;
+DataFile* df;
+MetadataPage* metadata;
+uint64_t global_id = 1;
+
+char DB_FILENAME[256] = "../databases";
+char DATA_FILENAME[256] = "../databases";
+char *history[MAX_HISTORY];
+int hist_len = 0;
+struct termios orig_termios;
